9ec0d532a599898189e80fa1f130afbf9c501ccf
Miray Durgun <miraydurgun67@gmail.com>
2024-01-17 14:43:02 +0300

    Başlık ve tasktan value alındı

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,18 +1,37 @@
+import { React } from "react";
+
 function TaskCreate() {
-    return (
-      <div className="task-create">
-        <h3>Lütfen TaskEkleyiniz</h3>
-        <form className="task-form">
-          <label className="task-label">Başlık</label>
-          <input className="task-input" />
-          {/* default hali type="text" geldiğinden sadece input yazdık */}
-          <label className="task-label">Task Griniz</label>
-          <textarea className="task-input" rows={5} />
-          <button className="task-button">Oluştur</button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default TaskCreate;
-  
+  const [title, setTitle] = useState("");
+  const [taskDesc, setTaskDec] = useState("");
+
+  const handleChange = (event) => {
+    //event, inputun içine girdiğimiz değere tekabül eder
+    setTitle(event.target.value);
+  };
+
+  const handleTaskChange = (event) => {
+    //event, inputun içine girdiğimiz değere tekabül eder
+    setTaskDec(event.target.value);
+  };
+
+  return (
+    <div className="task-create">
+      <h3>Lütfen TaskEkleyiniz</h3>
+      <form className="task-form">
+        <label className="task-label">Başlık</label>
+        <input value={title} onChange={handleChange} className="task-input" />
+        {/* default hali type="text" geldiğinden sadece input yazdık */}
+        <label className="task-label">Task Griniz</label>
+        <textarea
+          value={taskDesc}
+          onChange={handleTaskChange}
+          className="task-input"
+          rows={5}
+        />
+        <button className="task-button">Oluştur</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskCreate;
